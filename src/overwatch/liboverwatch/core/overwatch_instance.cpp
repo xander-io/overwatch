b8f5cef0e11f78115b6ab733c21eb2d8da256b25
Zack Anderson <zackanderson026@gmail.com>
2020-05-21 01:19:14 -0400

    Implemented shutdown flag; placeholders for multithreading

--- src/overwatch/liboverwatch/core/overwatch_instance.cpp
+++ src/overwatch/liboverwatch/core/overwatch_instance.cpp
@@ -1,45 +1,77 @@
 #include <iostream>
 #include <stdio.h>
+#include <signal.h>
 #include <string>
 
 #include "overwatch_instance.hpp"
-#include "arguments_parser.hpp"
 #include "logging.hpp"
 
 namespace overwatch::core
 {
-namespace
-{
-args_map_t const parse_args(int const &argc, char const *const *const &argv)
-{
-    LOG_DEBUG << "Parsing arguments passed in from terminal";
-    ArgumentsParser args_parser;
-    return args_parser.parse(argc, argv);
-}
-} // namespace
-
-OverwatchInstance::OverwatchInstance() {}
-
-int OverwatchInstance::run(int const &argc, char const *const *const &argv) noexcept
-{
-    int exit_code = EXIT_SUCCESS;
-    try
+    void OverwatchInstance::shutdown() noexcept
     {
-        //Create a base logger for logging arg parsing issues
-        common::logging::init_logging();
-        // Parse the args and convert to a config
-        //args_map_t const args = parse_args(argc, argv);
-
-        /* PLACEHOLDER -- Spawn additional threads here and join on them */
-    }
-    catch (std::exception const &e)
-    {
-        if (common::logging::logging_initialized())
+        // If the config and logger have been initialized, activate the shutdown signal
+        if (common::logging::logger_initialized() && config_)
         {
-            LOG_ERROR << e.what() << std::endl;
+            LOG_INFO << "Shutting down overwatch instance";
+            config_->shutdown = true;
         }
-        exit_code = EXIT_FAILURE;
     }
-    return exit_code;
-}
+
+    args_map_t const OverwatchInstance::parse_args_(int const &argc, char const *const *const &argv)
+    {
+        ArgumentsParser args_parser;
+        return args_parser.parse(argc, argv);
+    }
+
+    void OverwatchInstance::sleep_()
+    {
+        LOG_INFO << "Waiting for external shutdown signal...";
+        while (!config_->shutdown)
+        {
+            LOG_DEBUG << "Shutdown signal is not set";
+            sleep(1);
+        }
+    }
+
+    void OverwatchInstance::wait_on_threads_()
+    {
+        /* FTODO: Wait on threads to shutdown before continuing */
+        return;
+    }
+
+    OverwatchInstance::OverwatchInstance()
+        : config_{nullptr} {}
+
+    int OverwatchInstance::run(int const &argc, char const *const *const &argv) noexcept
+    {
+        int exit_code = EXIT_SUCCESS;
+        try
+        {
+            //Create a base logger for logging arg parsing issues
+            // Parse the args and convert to a config
+            args_map_t const args = parse_args_(argc, argv);
+            config_ = std::unique_ptr<Config>{new Config{args}};
+            common::logging::set_logger(config_->logging);
+            LOG_INFO << config_->to_string();
+            LOG_INFO << "Running overwatch...";
+            sleep_();
+            wait_on_threads_();
+        }
+        catch (std::exception const &e)
+        {
+            if (common::logging::logger_initialized())
+            {
+                LOG_ERROR << e.what() << std::endl;
+            }
+            // The logger will not be initialized if argparse fails
+            else
+            {
+                std::cout << e.what() << std::endl;
+            }
+            exit_code = EXIT_FAILURE;
+        }
+        return exit_code;
+    }
+
 } // namespace overwatch::core
--- src/overwatch/liboverwatch/core/overwatch_instance.hpp
+++ src/overwatch/liboverwatch/core/overwatch_instance.hpp
@@ -1,19 +1,27 @@
 #pragma once
 
 #include <unordered_map>
+#include <atomic>
 
-// #include "arguments_parser.hpp"
-// #include "config.hpp"
+#include "arguments_parser.hpp"
+#include "config.hpp"
 
 namespace overwatch::core
 {
-class OverwatchInstance
-{
-public:
-    OverwatchInstance();
-    int run(int const &argc, char const *const *const &argv) noexcept;
+    class OverwatchInstance
+    {
+    public:
+        OverwatchInstance();
+        int run(int const &argc, char const *const *const &argv) noexcept;
+        void shutdown() noexcept;
+        Config const get_config() noexcept;
 
-private:
-    // Config config_;
-};
+    private:
+        args_map_t const parse_args_(int const &argc, char const *const *const &argv);
+        //void init_*();
+        void sleep_();
+        void wait_on_threads_();
+
+        std::unique_ptr<Config> config_;
+    };
 } // namespace overwatch::core
