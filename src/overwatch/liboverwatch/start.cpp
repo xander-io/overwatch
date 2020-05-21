090ff8c2b7a48214fcb26d8d233a4ee01a272334
Zack Anderson <zackanderson026@gmail.com>
2020-05-21 01:16:24 -0400

    Added signal handling and cleanup

--- src/overwatch/liboverwatch/start.cpp
+++ src/overwatch/liboverwatch/start.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <signal.h>
 
 #include "start.hpp"
 #include "overwatch_instance.hpp"
 
 namespace overwatch
 {
-int start(int const &argc, char const *const *const &argv) noexcept
-{
-    overwatch::core::OverwatchInstance instance;
-    return instance.run(argc, argv);
-}
+    namespace
+    {
+        overwatch::core::OverwatchInstance instance;
+        void cleanup_(int) noexcept
+        {
+            instance.shutdown();
+        }
+
+    } // namespace
+
+    int start(int const &argc, char const *const *const &argv) noexcept
+    {
+        signal(SIGHUP, cleanup_);
+        signal(SIGINT, cleanup_);
+        signal(SIGTERM, cleanup_);
+        return instance.run(argc, argv);
+    }
 } // namespace overwatch
