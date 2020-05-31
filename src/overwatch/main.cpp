3f09c57e13fd9ff57adf62b80565d5919e9d4900
Zack Anderson <zackanderson026@gmail.com>
2020-05-31 13:53:42 -0400

    Refactored overwatch lib for interface access; Started unit tests

--- src/overwatch/main.cpp
+++ src/overwatch/main.cpp
@@ -16,19 +16,108 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <future>
+#include <signal.h>
+#include <iostream>
 
-#include "start.hpp"
+#include "config.hpp"
+#include "logging.hpp"
+#include "arguments_parser.hpp"
+
+namespace
+{
+    std::unique_ptr<overwatch::core::Config> config_{nullptr};
+
+    /**
+     * Clean up the overwatch instance once a signal is fired
+     * 
+     * @param[in] The signal
+     */
+    void cleanup_(int) noexcept
+    {
+        if (config_)
+        {
+            LOG_DEBUG << "Received external shutdown signal";
+            config_->shutdown = true;
+        }
+    }
+
+    /**
+     * Waits for the external shutdown
+     */
+    void sleep_() noexcept
+    {
+        LOG_INFO << "Waiting for external shutdown signal...";
+        while (!config_->shutdown)
+        {
+            LOG_DEBUG << "Shutdown signal is not set";
+            sleep(1);
+        }
+    }
+
+    overwatch::core::args_map_t const parse_args_(int const &argc, char const *const *const &argv)
+    {
+        overwatch::core::ArgumentsParser arg_parser;
+        return arg_parser.parse(argc, argv);
+    }
+
+    /**
+     * Initializes the signals for cleanup
+     */
+    void init_signals_() noexcept
+    {
+        LOG_DEBUG << "Adding cleanup signals";
+        signal(SIGHUP, cleanup_);
+        signal(SIGINT, cleanup_);
+        signal(SIGTERM, cleanup_);
+    }
+
+    /**
+     * Entry point for the overwatch executable
+     * 
+     * @param[in] argc Number of arguments
+     * @param[in] argv Argument values
+     * @return If the exe succeeds or fails
+     */
+    int overwatch_(int const &argc, char const *const *const &argv) noexcept
+    {
+        int exit_code = EXIT_SUCCESS;
+        try
+        {
+            // Parse the args and convert to a config
+            overwatch::core::args_map_t const args = parse_args_(argc, argv);
+            config_ = std::unique_ptr<overwatch::core::Config>{new overwatch::core::Config{args}};
+            common::logging::set_logger(config_->logging);
+            LOG_INFO << config_->to_string();
+            LOG_INFO << "Running overwatch...";
+            init_signals_();
+            sleep_();
+            //wait_on_threads_();
+        }
+        catch (std::exception const &e)
+        {
+            if (common::logging::logger_initialized())
+            {
+                LOG_ERROR << e.what() << std::endl;
+            }
+            // The logger will not be initialized if argparse fails
+            else
+            {
+                std::cout << e.what() << std::endl;
+            }
+            exit_code = EXIT_FAILURE;
+        }
+        return exit_code;
+    }
+} // namespace
 
 /**
  * Entry point for the overwatch executable
  * 
  * @param[in] argc Number of arguments
  * @param[in] argv Argument values
- * @return If the app succeeds or fails
+ * @return If the exe succeeds or fails
  */
 int main(int const argc, char const *const *const argv)
 {
-    std::future<int> exit_code = std::async(overwatch::start, argc, argv);
-    return exit_code.get();
+    return overwatch_(argc, argv);
 }
